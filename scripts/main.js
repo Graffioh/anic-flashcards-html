925c956517a87316aba88f17db6141d072af2978
Graffioh <bregliascuola2002@gmail.com>
2023-08-03 16:42:02 +0200

    Add front and rear, Add flip functionality

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@ const rearTextArea = document.getElementById("rear-textarea");
 const addBtn = document.getElementById("add-btn");
 const ulist = document.getElementById("flashcards-list-ul");
 
-var frontTxtStr = "";
-var rearTxtStr = "";
+var str = "";
 
 // Add typed text into unordered list by tapping on btn
 addBtn.addEventListener("click", () => {
@@ -14,21 +13,24 @@ addBtn.addEventListener("click", () => {
 });
 
 function addTextListNode() {
+    var frontTxtStr = "";
+    var rearTxtStr = "";
+
     frontTxtStr = frontTextArea.value;
     rearTxtStr = rearTextArea.value;
     
-    // for(let i = 0; i < rearTxtStr.length; i++) {
-    //     if(i % 37 === 0) {
-    //         rearTxtStr.at += "\n";
-    //     }
-    // }
-    // console.log rearTxtStr);
+    str = frontTxtStr;
 
     // If rearTxtStr has no empty value/whitespaces
     if (rearTxtStr.trim() && frontTxtStr.trim()) {
-        const paragraph = document.createElement("p");
+        const frontTextParagraph = document.createElement("p");
+        const rearTextParagraph = document.createElement("p");
         const ulistEntry = document.createElement("li");
         const deleteBtn = document.createElement("button");
+        const flipBtn = document.createElement("button");
+
+        rearTxtStr.replace(/\n/g, "<br />");
+        frontTxtStr.replace(/\n/g, "<br />");
 
         deleteBtn.setAttribute("class", "remove-btn-style");
         deleteBtn.innerText = "Remove";
@@ -36,11 +38,32 @@ function addTextListNode() {
             ulistEntry.remove()
         });
 
-        paragraph.innerHTML = rearTxtStr.replace(/\n/g, "<br />");
+        frontTextParagraph.style.display = "block";
+        rearTextParagraph.style.display = "none";
+        
+        frontTextParagraph.innerHTML = frontTxtStr;
+        rearTextParagraph.innerHTML = rearTxtStr;
 
-        ulistEntry.appendChild(paragraph);
+        flipBtn.setAttribute("class", "flip-btn-style");
+        flipBtn.innerText = "Flip";
+        flipBtn.addEventListener("click", function() {
+            if(frontTextParagraph.style.display === "none") {
+                console.log("Front should be visible");
+
+                frontTextParagraph.style.display = "block";
+                rearTextParagraph.style.display = "none";
+            } else {
+                console.log("Rear should be visible");
+
+                frontTextParagraph.style.display = "none";
+                rearTextParagraph.style.display = "block";
+            }
+        });
+
+        ulistEntry.appendChild(frontTextParagraph);
+        ulistEntry.appendChild(rearTextParagraph);
         ulistEntry.appendChild(deleteBtn);
-        ulistEntry.setAttribute("id", "card-1");
+        ulistEntry.appendChild(flipBtn);
 
         ulist.appendChild(ulistEntry);
         
